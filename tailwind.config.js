e18f8e2b7d0b26642c07ca8652fd3aa9e8ed3e67
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2024-11-23 23:18:25 +0200

    Update Tailwind config with tiny screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,17 +7,19 @@ export default {
   theme: {
     extend: {
       colors: {
-        primaryText: "",
-        secondaryText: "",
-        primaryBackground: "",
-        secondaryBackground: "",
+        primaryText: "#89A8B2",
+        secondaryText: "#B3C8CF",
+        primaryBackground: "#F1F0E8",
+        secondaryBackground: "#E5E1DA",
       },
       screens: {
         '2xl': '1536px',
-        'xl': '1280px',
-        'lg': '1024px',
-        'md': '768px',
-        'sm': '640px',
+        xl: "1280px",
+        lg: "1024px",
+        md: "868px",
+        sm: "640px",
+        vsm: "460px",
+        tiny: "360px",
       },
     },
   },
